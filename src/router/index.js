1176a53d9f75c6ceec6b2c31fb88fad3cd2b69ee
okborges <rafaelborges.av@gmail.com>
2022-11-12 21:41:58 -0300

    configurando vue 3

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,38 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+
+const HomeView = () => import('../views/HomeView/index.vue');
+const FeedbacksView = () => import('../views/FeedbacksView/index.vue');
+const CredencialsView = () => import('../views/CredencialsView/index.vue');
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'Home',
+      component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: '/feedbacks',
+      name: 'Feedbacks',
+      component: FeedbacksView,
+      meta: {
+        hasAuth: true,
+      },
+    },
+    {
+      path: '/credencials',
+      name: 'Credencials',
+      component: CredencialsView,
+      meta: {
+        hasAuth: true,
+      },
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: { name: 'Home' },
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ src/views/HomeView/index.vue
@@ -0,0 +1,10 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div>
+    <h1 class="text-brand-main font-black">Aqui é home</h1>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
