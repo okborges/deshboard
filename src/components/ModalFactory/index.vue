db36ffe6c81f26b1ee0c0cd8d92efff6800c072f
okborges <rafaelborges.av@gmail.com>
2023-02-02 21:54:01 -0300

    modal de longin

--- src/components/ModalFactory/index.vue
+++ src/components/ModalFactory/index.vue
@@ -62,11 +62,11 @@ export default {
       class="fixed top-0 left-0 z-50 flex items-center justify-center w-full h-full bg-black bg-opacity-50"
       @click="handleModalToogle({ status: false })"
     >
-      <div class="fixed mx-10" :class="state.width" @click.stop>
+      <div class="fixed" :class="state.width" @click.stop>
         <div
           class="flex flex-col overflow-hidden bg-white rounded-lg animate__animated animate__fadeInDown animate__faster"
         >
-          <div class="flex flex-col px-12 py-10 bg-white">
+          <div class="flex flex-col p-10 bg-white">
             <component :is="state.component" />
           </div>
         </div>
--- src/components/ModalLogin/index.vue
+++ src/components/ModalLogin/index.vue
@@ -1,12 +1,82 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <h1>Modal de login</h1>
+  <div class="flex justify-between">
+    <h1 class="text-4xl font-black text-gray-800">Entre na sua conta</h1>
+
+    <button class="text-4xl text-gray-600" @click="close">&times;</button>
+  </div>
+
+  <div class="mt-16">
+    <form @submit.prevent="handleSubmit">
+      <label class="block">
+        <span class="text-lg font-medium text-gray-800">E-mail</span>
+        <input
+          v-model="state.email.value"
+          type="email"
+          :class="{ 'border-brand-danger': !!state.email.errorMessage }"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          placeholder="melhor.email@gmail.com"
+        />
+        <span
+          v-if="!!state.email.errorMessage"
+          class="block font-medium text-brand-danger"
+          >{{ state.email.errorMessage }}</span
+        >
+      </label>
+
+      <label class="block">
+        <span class="text-lg font-medium text-gray-800">Senha</span>
+        <input
+          v-model="state.password.value"
+          type="password"
+          :class="{
+            'border-brand-danger': !!state.password.value.errorMessage,
+          }"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          placeholder="****"
+        />
+        <span
+          v-if="!!state.password.value.errorMessage"
+          class="block font-medium text-brand-danger"
+          >{{ state.password.value.errorMessage }}</span
+        >
+      </label>
+
+      <button
+        :disabled="state.isLoading"
+        type="submit"
+        :class="{ 'opacity-50': state.isLoading }"
+        class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main transition-all duration-150"
+      >
+        Entrar
+      </button>
+    </form>
+  </div>
 </template>
 
 <script>
+import { reactive } from 'vue';
+import useModal from '../../hooks/useModal';
+
 export default {
   setup() {
-    return {};
+    const modal = useModal();
+    const state = reactive({
+      hasErrors: false,
+      isLoading: false,
+      email: {
+        value: '',
+        errorMessage: '',
+      },
+      password: {
+        value: '',
+        errorMessage: '',
+      },
+    });
+
+    function handleSubmit() {}
+
+    return { state, close: modal.close };
   },
 };
 </script>
