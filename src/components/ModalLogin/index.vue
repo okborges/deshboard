5b3f50cfa905a380603e29227c8dac99c619d19a
okborges <rafaelborges.av@gmail.com>
2023-02-03 22:06:58 -0300

    validação

--- src/components/ModalLogin/index.vue
+++ src/components/ModalLogin/index.vue
@@ -1,9 +1,11 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-  <div class="flex justify-between">
+  <div class="flex justify-between" id="modal-login">
     <h1 class="text-4xl font-black text-gray-800">Entre na sua conta</h1>
 
-    <button class="text-4xl text-gray-600" @click="close">&times;</button>
+    <button @click="close" class="text-4xl text-gray-600 focus:outline-none">
+      &times;
+    </button>
   </div>
 
   <div class="mt-16">
@@ -11,44 +13,54 @@
       <label class="block">
         <span class="text-lg font-medium text-gray-800">E-mail</span>
         <input
+          id="email-field"
           v-model="state.email.value"
           type="email"
-          :class="{ 'border-brand-danger': !!state.email.errorMessage }"
-          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
+          :class="{
+            'border-brand-danger': !!state.email.errorMessage,
+          }"
+          class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 borderrorMessageer-2 border-transparent rounded"
           placeholder="melhor.email@gmail.com"
         />
         <span
+          id="email-error"
           v-if="!!state.email.errorMessage"
           class="block font-medium text-brand-danger"
-          >{{ state.email.errorMessage }}</span
         >
+          {{ state.email.errorMessage }}
+        </span>
       </label>
 
-      <label class="block">
+      <label class="block mt-5">
         <span class="text-lg font-medium text-gray-800">Senha</span>
         <input
+          id="password-field"
           v-model="state.password.value"
           type="password"
           :class="{
-            'border-brand-danger': !!state.password.value.errorMessage,
+            'border-brand-danger': !!state.password.errorMessage,
           }"
           class="block w-full px-4 py-3 mt-1 text-lg bg-gray-100 border-2 border-transparent rounded"
-          placeholder="****"
+          placeholder="••••••••"
         />
         <span
-          v-if="!!state.password.value.errorMessage"
+          v-if="!!state.password.errorMessage"
           class="block font-medium text-brand-danger"
-          >{{ state.password.value.errorMessage }}</span
         >
+          {{ state.password.errorMessage }}
+        </span>
       </label>
 
       <button
+        id="submit-button"
         :disabled="state.isLoading"
         type="submit"
-        :class="{ 'opacity-50': state.isLoading }"
-        class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main transition-all duration-150"
+        :class="{
+          'opacity-50': state.isLoading,
+        }"
+        class="px-8 py-3 mt-10 text-2xl font-bold text-white rounded-full bg-brand-main focus:outline-none transition-all duration-150"
       >
-        Entrar
+        <span>Entrar</span>
       </button>
     </form>
   </div>
@@ -56,27 +68,45 @@
 
 <script>
 import { reactive } from 'vue';
+import { useField } from 'vee-validate';
+import {
+  validateEmptyAndLength3,
+  validateEmptyAndEmail,
+} from '../../utils/validators';
 import useModal from '../../hooks/useModal';
 
 export default {
   setup() {
     const modal = useModal();
+
+    const { value: emailValue, errorMessage: emailErrorMessage } = useField(
+      'email',
+      validateEmptyAndEmail,
+    );
+
+    const { value: passwordValue, errorMessage: passwordErrorMessage } =
+      useField('password', validateEmptyAndLength3);
+
     const state = reactive({
       hasErrors: false,
       isLoading: false,
       email: {
-        value: '',
-        errorMessage: '',
+        value: emailValue,
+        errorMessage: emailErrorMessage,
       },
       password: {
-        value: '',
-        errorMessage: '',
+        value: passwordValue,
+        errorMessage: passwordErrorMessage,
       },
     });
 
     function handleSubmit() {}
 
-    return { state, close: modal.close };
+    return {
+      state,
+      close: modal.close,
+      handleSubmit,
+    };
   },
 };
 </script>
